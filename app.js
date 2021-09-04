8ebf0c3e5649137af2bae99b0f5ea9470efedb58
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-09-04 14:57:16 +0900

    feat: init project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+
+require("dotenv").config();
+
+const app = express();
+
+PORT = process.env.PORT;
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then((result) => {
+    console.log("DB Connected!");
+    app.listen(PORT, () => {
+      console.log(PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
