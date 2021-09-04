2f5a9b0eade2bc9930361736e8b6037fad8bccf9
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-09-04 21:42:02 +0900

    feat: login

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ const app = express();
 
 const authRoutes = require("./routes/auth");
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  next();
+});
+
+app.use(bodyParser.json());
+
 app.use("/auth", authRoutes);
 
 PORT = process.env.PORT;
--- controllers/auth.js
+++ controllers/auth.js
@@ -3,18 +3,27 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res, next) => {
-  const hashedPassword = await bcrypt.hash(req.body.password, 12);
+  console.log("signup route");
+  console.log(req.body);
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
 
-  const user = new User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    userName: req.body.userName,
-    email: req.body.email,
-    passwrod: hashedPassword,
-  });
+    const user = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      userName: req.body.userName,
+      email: req.body.email,
+      password: hashedPassword,
+    });
 
-  await user.save();
-  res.status(201).json({ message: "Successfully signed up!" });
+    await user.save();
+    res.status(201).json({ message: "Successfully signed up!" });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
 };
 
 exports.login = async (req, res, next) => {};
