37e7c5bc882b0e96544c308ee1bb4943e3e95d14
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-09-19 22:23:11 +0900

    refactor: User model

--- models/User.js
+++ models/User.js
@@ -14,10 +14,12 @@ const userSchema = new Schema({
   userName: {
     type: String,
     required: true,
+    unique: true,
   },
   email: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
